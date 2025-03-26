68b172fc7f85034b011babed7c745e518a123a50
Ali ihsan Cengiz <a.cengiz@saltosystems.com>
2025-03-26 14:34:21 +0100

    Implement model and validation set control

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1138,6 +1138,118 @@ def test_disconnect_interface(monkeypatch):
 
     assert result == mock_response
 
+def test_get_model(monkeypatch) -> None:
+    """`api.get_model` returns a `types.SnapdResponse`."""
+    mock_response = types.SnapdResponse(
+        type="sync",
+        status_code=200,
+        status="OK",
+        result={},
+    )
+
+    def mock_get(path):
+        assert path == "/model"
+
+        return mock_response
+
+    monkeypatch.setattr(http, "get", mock_get)
+
+    result = api.get_model()
+
+    assert result == mock_response
+
+def test_remodel(monkeypatch):
+    """`api.remodel` returns a `types.SnapdResponse`."""
+    mock_response = types.SnapdResponse(
+        type="async",
+        status_code=202,
+        status="OK",
+        result={},
+    )
+
+    def mock_post(path, body):
+        assert path == "/model"
+        assert body == {"new-model": "dummy_model_assertion", "offline" : True}
+        return mock_response
+
+    monkeypatch.setattr(http, "post", mock_post)
+
+    result = api.remodel("dummy_model_assertion", True)
+
+    assert result == mock_response
+
+
+def test_get_validation_sets(monkeypatch) -> None:
+    """`api.get_validation_sets` returns a `types.SnapdResponse`."""
+    mock_response = types.SnapdResponse(
+        type="sync",
+        status_code=200,
+        status="OK",
+        result={},
+    )
+
+    def mock_get(path):
+        assert path == "/validation-sets"
+        return mock_response
+
+    monkeypatch.setattr(http, "get", mock_get)
+
+    result = api.get_validation_sets()
+
+    assert result == mock_response
+
+
+def test_get_validation_set(monkeypatch) -> None:
+    """`api.get_validation_set` returns a `types.SnapdResponse`."""
+    mock_response = types.SnapdResponse(
+        type="sync",
+        status_code=200,
+        status="OK",
+        result={},
+    )
+
+    account_id: str = "device-platform"
+    validation_set_name: str = "dev-validation-set"
+    def mock_get(path):
+        assert path == f"/validation-sets/{account_id}/{validation_set_name}"
+        return mock_response
+
+    monkeypatch.setattr(http, "get", mock_get)
+
+    result = api.get_validation_set(account_id, validation_set_name)
+
+    assert result == mock_response
+
+def test_refresh_validation_set(monkeypatch) -> None:
+    """`api.refresh_validation_set` returns a `types.SnapdResponse`."""
+    mock_response = types.SnapdResponse(
+        type="async",
+        status_code=202,
+        status="OK",
+        result={},
+    )
+
+    account_id: str = "device-platform"
+    validation_set_name: str = "dev-validation-set"
+    validation_set_sequence: int = 12
+    def mock_post(path, body):
+        assert path == "/snaps"
+        assert body == {
+                "action": "refresh",
+                "validation-sets": [
+                    f"{account_id}/{validation_set_name}={validation_set_sequence}"
+                    ],
+                }
+
+        return mock_response
+
+    monkeypatch.setattr(http, "post", mock_post)
+
+    result = api.refresh_validation_set(account_id, validation_set_name, validation_set_sequence)
+
+    assert result == mock_response
+
+
 def test_get_assertion_types(monkeypatch):
     """`api.get_assertion_types` returns a `types.SnapdResponse`."""
     mock_response = types.SnapdResponse(
