892178002c970bc26c1b0ac36a7f7a7da52eedc7
Ali ihsan Cengiz <aliihsan_29@hotmail.com>
2025-04-23 10:51:01 +0200

    Implement api for systems endpoint

--- snap_http/api.py
+++ snap_http/api.py
@@ -447,6 +447,42 @@ def refresh_validation_set(account_id: str, validation_set_name: str, validation
     }
     return http.post("/snaps", body=body)
 
+# System: Get and perform action with recovery system
+def get_recovery_systems() -> SnapdResponse:
+    """
+    GET all recovery systems
+    :return: A SnapdResponse containing the response from the snapd API.
+    """
+    return http.get("/systems")
+
+def perform_system_action(action: str, mode: str)-> SnapdResponse:
+    """
+    Attempt to perform an action with the current active recovery system.
+    :param action: Action to perform, which is either “reboot”, “create” or “do”.
+    :param mode:  The mode to transition to either "run", "recover", "install" or "factory-reset".
+    :return: A SnapdResponse containing the response from the snapd API.
+    """
+    body = {
+        "action": action,
+        "mode": mode
+    }
+    return http.post("/systems", body=body)
+
+def perform_recovery_action(label: str, action: str, mode: str)-> SnapdResponse:
+    """
+    Attempt to perform an action with the current active recovery system.
+    :param label: Label to specify recovery system.
+    :param action: Action to perform, which is either “reboot”, “create” or “do”.
+    :param mode:  The mode to transition to either "run", "recover", "install" or "factory-reset".
+    :return: A SnapdResponse containing the response from the snapd API.
+    """
+    body = {
+        "action": action,
+        "mode": mode
+    }
+    return http.post(f"/systems/{label}", body=body)
+
+
 
 # Assertions: list and add assertions
 
