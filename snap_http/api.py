7bcb4f14a68341290bb27372d310615ff5bf260e
mcw-work <111986660+mcw-work@users.noreply.github.com>
2025-07-11 19:14:14 +0100

    v1.8.0 (#20)

--- snap_http/api.py
+++ snap_http/api.py
@@ -219,14 +219,66 @@ def revert_all(names: List[str]) -> SnapdResponse:
     return http.post("/snaps", {"action": "revert", "snaps": names})
 
 
-def remove(name: str) -> SnapdResponse:
+def remove(name: str,
+           purge: Optional[bool] = False,
+           terminate: Optional[bool] = False) -> SnapdResponse:
     """Uninstalls a snap identified by `name`."""
-    return http.post("/snaps/" + name, {"action": "remove"})
+    body = {
+        "action": "remove",
+        "purge": purge,
+        "terminate": terminate,
+    }
+
+    return http.post("/snaps/" + name, body)
 
 
 def remove_all(names: List[str]) -> SnapdResponse:
     """Uninstalls all snaps identified in `names`."""
     return http.post("/snaps", {"action": "remove", "snaps": names})
+
+
+def snapshots() -> SnapdResponse:
+    """Gets a list of all snapshots."""
+    return http.get("/snapshots")
+
+
+def forget_snapshot(id: str, snaps: Optional[List[str]] = None, users: Optional[List[str]] = None) -> SnapdResponse:
+    """Deletes a snapshot identified by `id`.
+
+    :param snap_id: The ID of the snapshot to delete.
+    """
+
+    body: Dict[str, Union[str, List[str]]] = {
+        "action": "forget",
+        "set": id
+    }
+
+    if snaps is not None:
+        body["snaps"] = snaps
+    if users is not None:
+        body["users"] = users
+
+    return http.post("/snapshots", body)
+
+
+def save_snapshot(
+    snaps: Optional[List[str]] = None,
+    users: Optional[List[str]] = None,
+) -> SnapdResponse:
+    """Saves a snapshot of the current state of the system.
+
+    :param name: The name of the snapshot.
+    :param users:  array of user names to whom snapshots are to be restricted .
+    :param snaps: Optional list of snaps to include in the snapshot.
+    """
+    body: Dict[str, Union[str, List[str]]] = {"action": "snapshot"}
+
+    if users is not None:
+        body["users"] = users
+    if snaps is not None:
+        body["snaps"] = snaps
+
+    return http.post("/snaps", body)
 
 
 def switch(name: str, *, channel: str = "stable") -> SnapdResponse:
@@ -261,6 +313,11 @@ def list() -> SnapdResponse:
     """
     return http.get("/snaps")
 
+
+def list_all() -> SnapdResponse:
+    """GETs a list of all installed snaps including disabled ones.
+    """
+    return http.get("/snaps?select=all")
 
 # Configuration: get and set snap options
 
