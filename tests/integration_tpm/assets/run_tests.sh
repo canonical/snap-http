037fae52c6cb757bbd46c477715d4348de0b4d5d
Justin Kim <justin.kim@canonical.com>
2026-01-26 10:25:58 -0800

    clean up, run weekly

--- tests/integration_tpm/assets/run_tests.sh
+++ tests/integration_tpm/assets/run_tests.sh
@@ -1,6 +1,9 @@
 sudo snap version
+# mount current directory
 sudo mkdir -p /mnt/host
 mountpoint -q /mnt/host || sudo mount -t 9p -o trans=virtio hostshare /mnt/host
 cd /mnt/host/
+
+# run integration_tpm tests
 sudo poetry install
 sudo poetry run pytest tests/integration_tpm
