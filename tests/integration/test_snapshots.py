b1c312dc6f02edb4a63a061f9d9fd643b5899d28
Stephen Mwangi <mail@stephenmwangi.com>
2026-02-20 16:02:01 +0300

    refactor(tests): return change response from wait_for

--- tests/integration/test_snapshots.py
+++ tests/integration/test_snapshots.py
@@ -5,7 +5,7 @@ from tests.utils import wait_for
 
 def test_save_snapshot(test_snap):
     """Test saving a snapshot."""
-    response = wait_for(snap_http.save_snapshot)(snaps=["snapd"])
+    response, _ = wait_for(snap_http.save_snapshot)(snaps=["snapd"])
     assert response.status_code == 202
 
     set_id = response.result["set-id"]
@@ -15,14 +15,14 @@ def test_save_snapshot(test_snap):
 
 def test_forget_snapshot(test_snap):
     """Test forgetting a snapshot."""
-    response = wait_for(snap_http.save_snapshot)(snaps=["snapd"])
+    response, _ = wait_for(snap_http.save_snapshot)(snaps=["snapd"])
     assert response.status_code == 202
 
     set_id = response.result["set-id"]
     snapshots = snap_http.snapshots().result
     assert any(shot['id'] == set_id for shot in snapshots)
 
-    response = wait_for(snap_http.forget_snapshot)(set_id)
+    response, _ = wait_for(snap_http.forget_snapshot)(set_id)
     assert response.status_code == 202
 
     snapshots = snap_http.snapshots().result
